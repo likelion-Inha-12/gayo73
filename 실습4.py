d3fda302c9800c3fe793cf623d5f49adf9982ecc
gayo73 <163620291+gayo73@users.noreply.github.com>
2024-03-27 22:18:46 +0900

    Delete 실습4.py

--- 실습4.py
+++
@@ -1,8 +0,0 @@
-list1=["김기현", "정보통신공학과",24, "infj"]
-list2=["김민경", "컴퓨터공학과",24, "esfj"]
-list3=["심성민", "일어일문학과",26 , "infj"]
-
-team = {'t1': list1, 't2': list2, 't3': list3}
-print (team['t1'])
-print (team['t2'])
-print (team['t3'])
