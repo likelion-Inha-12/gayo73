097e57a42544b46aa5cf1fc5ca005a1d3b2d4ea0
gayoung <gayoung2037@gmail.com>
2024-04-30 13:12:15 +0900

    장고 모델 과제

---
+++ semi_pro/lionapp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('create/',views.create_post),
+    path('<int:pk>/', views.get_post),
+    path('delete/<int:pk>',views.delete_post),
+    path('comments/<int:post_id>', views.get_comment),
+    path('member/', views.create_member),
+    path('like/<int:user_id>/<int:post_id>', views.like_post),
+    path('getlikes/<int:post_id>', views.get_likes),
+    path('sort', views.sort_post)
+
+]
