eae01e93c6d2c74460d3ee8ce841c57a1fe50423
gayoung <gayoung2037@gmail.com>
2024-05-04 23:37:47 +0900

    장고 5주차

---
+++ Desktop/django-week5/semi_pro/lionapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from lionapp import views
+
+urlpatterns = [
+    path('create/',views.create_post),
+    path('post/<int:pk>/',views.get_post),
+    path('delete/<int:pk>/',views.delete_post),
+    path('v2/post/<int:pk>/',views.PostApiView.as_view()),
+    path('v2/post',views.create_post_v2)
+]
